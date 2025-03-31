318845ae3f0cb42ab1dea71672084fb2d9906a14
ghores <ghazimoradi.amin@gmail.com>
2025-03-31 12:21:55 +0330

    Implementation fragment note ui

---
+++ app/src/main/java/com/example/todoappmvp/ui/add/NoteFragment.kt
@@ -0,0 +1,27 @@
+package com.example.todoappmvp.ui.add
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.todoappmvp.R
+import com.example.todoappmvp.databinding.FragmentNoteBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class NoteFragment : BottomSheetDialogFragment() {
+    //Binding
+    private lateinit var binding: FragmentNoteBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentNoteBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //InitView
+        binding.apply {
+
+        }
+    }
+}
